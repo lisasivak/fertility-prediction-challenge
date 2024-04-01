573673ee95040ccebb2bd5f89fd29ce8209f4cef
lisasivak <30319803+lisasivak@users.noreply.github.com>
2024-04-01 18:33:16 +0200

    check more packages

--- packages.R
+++ packages.R
@@ -1,3 +1,4 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("caret","data.table","tidyr","xgboost","e1071","randomForest","lubridate"), repos="https://cran.r-project.org")
+install.packages(c("caret","data.table","tidyr","xgboost","e1071","randomForest","lubridate","ggplot2",
+"ggraph","dplyr","mlr3",), repos="https://cran.r-project.org")
