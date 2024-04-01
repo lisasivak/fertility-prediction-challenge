b951ee0223688602885cd47d5f275b021ac33c00
lisasivak <30319803+lisasivak@users.noreply.github.com>
2024-04-01 22:55:30 +0200

    example updating factor level

--- packages.R
+++ packages.R
@@ -1,4 +1,4 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("caret","data.table","tidyr","xgboost","e1071","randomForest","lubridate","ggplot2",
+install.packages(c("caret","glmnet","data.table","tidyr","xgboost","e1071","randomForest","lubridate","ggplot2",
 "ggraph","dplyr","mlr3"), repos="https://cran.r-project.org")
--- submission.R
+++ submission.R
@@ -1,33 +1,69 @@
 library(dplyr)
+library(glmnet)
+library(caret)
 ##
 clean_df <- function(df, background_df = NULL){
   
-  # creating average income for all respondents
-  income_3months <- background_df %>% 
-    # only select last 3 months
-    filter(wave >= 202010 & wave <= 202012) %>% 
-    # for each respondent
-    group_by(nomem_encr) %>% 
-    # calculate average income
-    summarise(mean_income = mean(netinc, na.rm = TRUE))
-  
-  # add income to train data
-  data <- left_join(df, income_3months, by = "nomem_encr")
-  data <- data %>% 
-    # impute mean income if missing
-    mutate(mean_income_imp = if_else(is.na(mean_income), 
-                                     mean(mean_income, na.rm = TRUE),
-                                     mean_income))
-  
-  data$age <- 2024 - data$birthyear_bg
-  
+  # select only cases where outcome is available 
+  df <- df %>% filter(outcome_available == 1)
+ 
+  ## Selecting variables
   keepcols = c('nomem_encr', # ID variable required for predictions,
-               'age', 
-               'mean_income_imp')  
+               'birthyear_bg', # birthyear of respondents
+               'gender_bg', # gender of respondents, factor
+               'oplmet_2020',# highest educational level in 2020
+               'cf20m024', # having a partner
+               'cf20m180'  # satisfaction with relationships
+               ) 
   
-  data <- data[ , keepcols ]
+  ## Keeping data with variables selected
+  df <- df %>% select(all_of(keepcols))
+  ## function for getting mode
+  mode <- function(x) {
+    x <- x[ !is.na(x) ]
+    ux <- unique(x)
+    tab <- tabulate(match(x, ux))
+    mode <- ux[tab == max(tab)]
+    ifelse(length(mode) > 1, sample(mode, 1), mode)
+  }
   
-  return(data)
+  # impute missing values with mode for other categorical variables and mean for continuous
+  df <- df %>% 
+    mutate(
+      birthyear_bg = ifelse(is.na(birthyear_bg), 
+                            mean(birthyear_bg, na.rm = TRUE), birthyear_bg),
+      gender_bg = ifelse(is.na(gender_bg),
+                         mode(gender_bg), gender_bg),
+      oplmet_2020 = ifelse(is.na(oplmet_2020),
+                           mode(oplmet_2020), oplmet_2020),
+      # add category "no partner" - 99 - in case there is no partner (cf20m024 == 2 )
+      cf20m180 = case_when(cf20m024 == 2 ~ 99,
+                           is.na(cf20m180) ~ 999, # other NAs impute with a new category 999
+                           TRUE ~cf20m180),
+      cf20m024 = ifelse(is.na(cf20m024), 999, cf20m024)
+      
+    ) %>% 
+    # standardise continuous variable, create factor for categorical variables
+    # needed for glmnet
+    mutate(
+      birthyear_bg = as.numeric(scale(birthyear_bg)), # z-scores, as.numeric to remove attributes
+      gender_bg = factor(gender_bg),
+      oplmet_2020 = factor(oplmet_2020),
+      cf20m180 = factor(cf20m180),
+      cf20m024 = factor(cf20m024)
+    )
+  
+
+  ##################
+  # updating factor levels to keep all columns in the data when the variable will be turned into dummy 
+  # because some categories are missing now in the train data which are present in the fake (and likely holdout) data -- see description.md
+  levels(df$cf20m180) <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10","99","999")  
+  ####################
+
+  # turn factors into dummy variables, required for glmnet
+  df <- model.matrix(~ ., df)
+  
+  return(df)
 }
 
 predict_outcomes <- function(df, background_df = NULL, model_path = "./model.rds"){
