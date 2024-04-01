878b6f73678ad4059c584ac77886765f4ae49c7f
lisasivak <30319803+lisasivak@users.noreply.github.com>
2024-04-01 18:30:36 +0200

    check packages

--- packages.R
+++ packages.R
@@ -1,3 +1,3 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("tidyverse","data.table","tidyr"), repos="https://cran.r-project.org")
+install.packages(c("caret","data.table","tidyr","xgboost","e1071","randomForest","lubridate"), repos="https://cran.r-project.org")
