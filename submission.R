553771f327d4eaa3c755757bcf51062a501dbc41
Elizaveta Sivak <30319803+lisasivak@users.noreply.github.com>
2024-04-23 10:15:49 +0200

    Update submission.R
    testing example model - with correct model.rds

--- submission.R
+++ submission.R
@@ -1,73 +1,25 @@
-library(dplyr)
-library(glmnet)
-library(caret)
-###
 clean_df <- function(df, background_df = NULL){
+  # Preprocess the input dataframe to feed the model.
+  ### If no cleaning is done (e.g. if all the cleaning is done in a pipeline) leave only the "return df" command
   
-  # select only cases where outcome is available 
-  df <- df %>% filter(outcome_available == 1)
- 
-  ## Selecting variables
+  # Parameters:
+  # df (dataframe): The input dataframe containing the raw data (e.g., from PreFer_train_data.csv or PreFer_fake_data.csv).
+  # background (dataframe): Optional input dataframe containing background data (e.g., from PreFer_train_background_data.csv or PreFer_fake_background_data.csv).
+  
+  # Returns:
+  # data frame: The cleaned dataframe with only the necessary columns and processed variables.
+  
+  ## This script contains a bare minimum working example
+  # Create new age variable
+  df$age <- 2024 - df$birthyear_bg
+  
+  # Selecting variables for modelling
+  
   keepcols = c('nomem_encr', # ID variable required for predictions,
-               'birthyear_bg', # birthyear of respondents
-               'gender_bg', # gender of respondents, factor
-               'oplmet_2020',# highest educational level in 2020
-               'cf20m024', # having a partner
-               'cf20m180'  # satisfaction with relationships
-               ) 
+               'age')        # newly created variable
   
   ## Keeping data with variables selected
-  df <- df %>% select(all_of(keepcols))
-  ## function for getting mode
-  mode <- function(x) {
-    x <- x[ !is.na(x) ]
-    ux <- unique(x)
-    tab <- tabulate(match(x, ux))
-    mode <- ux[tab == max(tab)]
-    ifelse(length(mode) > 1, sample(mode, 1), mode)
-  }
-  
-  # impute missing values with mode for other categorical variables and mean for continuous
-  df <- df %>% 
-    mutate(
-      birthyear_bg = ifelse(is.na(birthyear_bg), 
-                            mean(birthyear_bg, na.rm = TRUE), birthyear_bg),
-      age = 2020 - birthyear_bg,
-      gender_bg = ifelse(is.na(gender_bg),
-                         mode(gender_bg), gender_bg),
-      oplmet_2020 = ifelse(is.na(oplmet_2020),
-                           mode(oplmet_2020), oplmet_2020),
-      # add category "no partner" - 99 - in case there is no partner (cf20m024 == 2 )
-      cf20m180 = case_when(cf20m024 == 2 ~ 99,
-                           is.na(cf20m180) ~ 999, # other NAs impute with a new category 999
-                           TRUE ~cf20m180),
-      cf20m024 = ifelse(is.na(cf20m024), 999, cf20m024)
-      
-    ) %>% 
-    # standardise continuous variable, create factor for categorical variables
-    # needed for glmnet
-    mutate(
-      age_st = as.numeric(scale(age)), # z-scores, as.numeric to remove attributes
-      gender_bg = factor(gender_bg),
-      oplmet_2020 = factor(oplmet_2020),
-      cf20m180 = factor(cf20m180),
-      cf20m024 = factor(cf20m024)
-    )
-  
-  
-  # updating factor levels to keep all columns in the data when the variable will be turned into dummy - even if some categories are missing
-  levels(df$cf20m180) <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10","99","999")  
-  
-  # add fake external data to test
-  external <- read.csv('fake_external_data.csv')
-  
-  df <- left_join(df, external, by = 'age')
-  
-  # remove additional columns for age
-  df <- df %>% select(-c(age, birthyear_bg))
-  
-  # turn factors into dummy variables, required for glmnet
-  df <- model.matrix(~ ., df)
+  df <- df[ , keepcols ]
   
   return(df)
 }
